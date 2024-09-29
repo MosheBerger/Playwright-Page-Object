f164e630ce7c3c03fe068e8ac3aaab41a8339cfb
Meby123B <meby123b@gmail.com>
2024-09-30 01:43:45 +0300

    Remove

--- global/locators/saveMessage.locator.ts
+++
@@ -1,8 +0,0 @@
-const SAVE_MESSAGE_LOCATORS = {
-    SAVED:'alert[type="success"]',
-    BUSY:'',
-    FAILED:'',
-    TEXT_CONTENT:'alert .ng-scope',
-}
-
-export default SAVE_MESSAGE_LOCATORS
--- src/explicitLocators/typography.ts
+++
@@ -1,7 +0,0 @@
-import { BaseLocator } from "../base/base.locator";
-import { Locator } from "../types";
-
-
-export class TypographyLocator extends BaseLocator {
-    innerText: Locator['innerText'] = this.$.innerText.bind(this.$)
-}
